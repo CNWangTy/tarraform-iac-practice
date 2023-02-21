585fbee263170c69e2763655401bbc94140c8fc1
Tianyi.Wang <tianyi.wang@thoughtworks.com>
2023-02-21 22:43:52 +0800

    [tianyi] fix issue,add infra for stable

---
+++ terraform/kind/layer2/stable/main.tf
@@ -0,0 +1,63 @@
+locals {
+  namespace   = "kind-stable"
+  platform    = "kind"
+  environment = "stable"
+}
+
+variable "db_password" {}
+
+resource "kubernetes_namespace" "namespace" {
+  metadata {
+    name = local.namespace
+  }
+}
+
+module "mysqldb" {
+  depends_on = [kubernetes_namespace.namespace]
+
+  source = "../../../module/mysql"
+
+  platform    = local.platform
+  namespace   = local.namespace
+  db_password = var.db_password
+}
+
+module "nginx-ingress" {
+  depends_on = [kubernetes_namespace.namespace]
+
+  source = "./ingress"
+
+  environment = local.environment
+  namespace   = local.namespace
+}
+
+module "book-service" {
+  depends_on = [module.mysqldb]
+
+  source    = "../../../../apps/book-service/terraform/kind-local"
+  namespace = local.namespace
+
+  db_host     = module.mysqldb.mysql_db_host
+  db_password = module.mysqldb.mysql_db_password
+  db_port     = module.mysqldb.mysql_db_port
+  db_user     = module.mysqldb.mysql_db_user
+}
+
+module "order-service" {
+  depends_on = [module.mysqldb]
+
+  source    = "../../../../apps/order-service/terraform/kind-local"
+  namespace = local.namespace
+
+  db_host     = module.mysqldb.mysql_db_host
+  db_password = module.mysqldb.mysql_db_password
+  db_port     = module.mysqldb.mysql_db_port
+  db_user     = module.mysqldb.mysql_db_user
+}
+
+module "web-app" {
+  depends_on = [module.mysqldb]
+
+  source    = "../../../../apps/web-app/terraform/kind-local"
+  namespace = local.namespace
+}
